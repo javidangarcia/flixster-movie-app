42d9d0a308550dd69eb6b030936a387464cfae79
Javier Garcia <javierdangarcia@meta.com>
2023-06-15 12:01:12 -0700

    added search bar that displays movies based on user input

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const poster_size = "w300";
 
 var currentPage = 1;
 
-document.addEventListener('DOMContentLoaded', fetchData);
-moviesBtn.addEventListener('click', fetchData);
+document.addEventListener('DOMContentLoaded', displayNowPlaying);
+moviesBtn.addEventListener('click', displayNowPlaying);
 
-async function fetchData() {
+async function displayNowPlaying() {
     try {
         const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${currentPage}`;
         const response = await fetch(url);
@@ -57,3 +57,57 @@ async function fetchData() {
         console.log(error);
     }
 };
+
+var submitBtn = document.getElementById("submitBtn");
+var searchInput = document.getElementById("search-input");
+
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    displaySearchMovie();
+});
+
+async function displaySearchMovie() {
+
+    try {
+        const searchQuery = searchInput.value;
+        console.log(searchQuery);
+        const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
+        const response = await fetch(searchUrl);
+        const data = await response.json();
+        const results = data["results"];
+        
+        movies.innerHTML = '';
+        results.forEach((movie) => {
+
+          const title = movie["title"];
+          const votes = movie["vote_average"];
+          const poster_path = movie["poster_path"];
+
+          // Creating a div for each movie and adding a class for styling
+          const movieDiv = document.createElement("div");
+          movieDiv.classList.add("movie-card")
+            
+          // Creating an img element for each movie poster
+          const img = document.createElement("img");
+          img.src = base_url + poster_size + '/' + poster_path;
+          img.classList.add("movie-poster");
+        
+          // Creating p tags that contain votes and title information
+          const votesParagraph = document.createElement("p");
+          votesParagraph.classList.add("movie-votes")
+          votesParagraph.textContent = votes;
+          const titleParagraph = document.createElement("p");
+          titleParagraph.classList.add("movie-title")
+          titleParagraph.textContent = title
+
+          movieDiv.appendChild(img);
+          movieDiv.appendChild(votesParagraph);
+          movieDiv.appendChild(titleParagraph);
+
+          movies.appendChild(movieDiv);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
