88452d9b66d2af8583c6ba367e2374d89414ba74
Javier Garcia <javierdangarcia@meta.com>
2023-06-15 16:18:54 -0700

    organized code by creating a displayMovies function

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 var movies = document.getElementById("movies-grid");
 var moviesBtn = document.getElementById("load-more-movies-btn");
+var searchMovies = document.getElementById("search-movies")
+var submitBtn = document.getElementById("submitBtn");
+var searchInput = document.getElementById("search-input");
 
 const apiKey = "43c1cf15af2eab2d2eb205ea044360be";
-
-// movie poster image link information
-const base_url = "http://image.tmdb.org/t/p/";
-const poster_size = "w300";
-
-var currentPage = 1;
+const baseUrl = "http://image.tmdb.org/t/p/";
+const posterSize = "w300";
 
 document.addEventListener('DOMContentLoaded', displayNowPlaying);
 moviesBtn.addEventListener('click', displayNowPlaying);
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    displaySearchMovie();
+});
+
+function displayMovies(results) {
+
+    results.forEach((movie) => {
+
+        const title = movie["title"];
+        const votes = movie["vote_average"];
+        const posterPath = movie["poster_path"];
+
+        // Creating a div for each movie and adding a class for styling
+        const movieDiv = document.createElement("div");
+        movieDiv.classList.add("movie-card")
+          
+        // Creating an img element for each movie poster
+        const img = document.createElement("img");
+        img.src = baseUrl + posterSize + '/' + posterPath;
+        img.classList.add("movie-poster");
+      
+        // Creating p tags that contain votes and title information
+        const votesParagraph = document.createElement("p");
+        votesParagraph.classList.add("movie-votes")
+        votesParagraph.textContent = votes;
+        const titleParagraph = document.createElement("p");
+        titleParagraph.classList.add("movie-title")
+        titleParagraph.textContent = title
+
+        movieDiv.appendChild(img);
+        movieDiv.appendChild(votesParagraph);
+        movieDiv.appendChild(titleParagraph);
+
+        movies.appendChild(movieDiv);
+
+    });
+        
+}
+
+var currentPage = 1;
 
 async function displayNowPlaying() {
     try {
@@ -19,37 +59,7 @@ async function displayNowPlaying() {
         const data = await response.json();
         const results = data["results"];
         
-        results.forEach((movie) => {
-
-          const title = movie["title"];
-          const votes = movie["vote_average"];
-          const poster_path = movie["poster_path"];
-
-          // Creating a div for each movie and adding a class for styling
-          const movieDiv = document.createElement("div");
-          movieDiv.classList.add("movie-card")
-            
-          // Creating an img element for each movie poster
-          const img = document.createElement("img");
-          img.src = base_url + poster_size + '/' + poster_path;
-          img.classList.add("movie-poster");
-        
-          // Creating p tags that contain votes and title information
-          const votesParagraph = document.createElement("p");
-          votesParagraph.classList.add("movie-votes")
-          votesParagraph.textContent = votes;
-          const titleParagraph = document.createElement("p");
-          titleParagraph.classList.add("movie-title")
-          titleParagraph.textContent = title
-
-          movieDiv.appendChild(img);
-          movieDiv.appendChild(votesParagraph);
-          movieDiv.appendChild(titleParagraph);
-
-          movies.appendChild(movieDiv);
-
-        
-        });
+        displayMovies(results);
 
         currentPage += 1;
 
@@ -58,54 +68,15 @@ async function displayNowPlaying() {
     }
 };
 
-var submitBtn = document.getElementById("submitBtn");
-var searchInput = document.getElementById("search-input");
-
-submitBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    displaySearchMovie();
-});
-
 async function displaySearchMovie() {
-
     try {
         const searchQuery = searchInput.value;
-        console.log(searchQuery);
-        const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
-        const response = await fetch(searchUrl);
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
+        const response = await fetch(url);
         const data = await response.json();
         const results = data["results"];
-        
-        movies.innerHTML = '';
-        results.forEach((movie) => {
 
-          const title = movie["title"];
-          const votes = movie["vote_average"];
-          const poster_path = movie["poster_path"];
-
-          // Creating a div for each movie and adding a class for styling
-          const movieDiv = document.createElement("div");
-          movieDiv.classList.add("movie-card")
-            
-          // Creating an img element for each movie poster
-          const img = document.createElement("img");
-          img.src = base_url + poster_size + '/' + poster_path;
-          img.classList.add("movie-poster");
-        
-          // Creating p tags that contain votes and title information
-          const votesParagraph = document.createElement("p");
-          votesParagraph.classList.add("movie-votes")
-          votesParagraph.textContent = votes;
-          const titleParagraph = document.createElement("p");
-          titleParagraph.classList.add("movie-title")
-          titleParagraph.textContent = title
-
-          movieDiv.appendChild(img);
-          movieDiv.appendChild(votesParagraph);
-          movieDiv.appendChild(titleParagraph);
-
-          movies.appendChild(movieDiv);
-        });
+        displayMovies(results);
 
     } catch (error) {
         console.log(error);
